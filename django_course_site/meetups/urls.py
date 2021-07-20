6c56056858ea6b2e4c1598c53251955149897ef0
aashishpnt <aashishpnt@gmail.com>
2021-07-20 15:59:16 +0545

    completed upto database

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups', views.index),
-    path('meetups/<slug:meetup_slug>', views.meetup_detail)
+    path('meetups/', views.index, name="index"),
+    path('meetups/<slug:meetup_slug>', views.meetup_detail, name="meetup_detail")
 ]
